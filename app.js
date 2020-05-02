da9779a322529ef999aa5a5351070ca7e33ea82f
jfilipek1 <47739228+jfilipek1@users.noreply.github.com>
2020-05-02 14:10:47 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var instasearchApp = new Vue({
+	
+	el: '#app-instasearch',
+	
+	data: { 
+		authorNameSearchString: "",
+		photoFeed: null
+	},
+	
+	mounted() {
+		axios
+			.get('https://open-football-apis.github.io/open-football-api/teams.json')
+			.then(response => {
+				this.photoFeed = response.data;
+			})
+			.catch(error => console.log(error))
+	},
+	
+	 computed: {
+        
+        filteredPhotoFeed: function () {
+			
+            var photos = this.photoFeed;
+            var authorNameSearchString = this.authorNameSearchString;
+
+            if(!authorNameSearchString){
+                return photos;
+            }
+
+            searchString = authorNameSearchString.trim().toLowerCase();
+
+            photos = photos.filter(function(item){
+                if(item.name.toLowerCase().indexOf(authorNameSearchString) !== -1){
+                    return item;
+                }
+            })
+
+            return photos;
+        }
+	 }
+	
+});
